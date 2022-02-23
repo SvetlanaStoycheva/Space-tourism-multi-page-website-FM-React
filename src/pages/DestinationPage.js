832e2c47c1123ba827167d1cb3b8e0a9d6b6e21a
Svetlana Stoycheva <s.stoycheva@gmail.com>
2022-02-23 17:23:58 +0200

    destination page completed

--- src/pages/DestinationPage.js
+++ src/pages/DestinationPage.js
@@ -9,40 +9,56 @@ function DestinationPage() {
     acc.push(curr.name);
     return acc;
   }, []);
-  console.log(buttons);
+  console.log(name, buttons);
+
+  //set active css on the clicket button, like moon;
 
   return (
     <section className='destination-page-container'>
-      <h4 className='subheading1'>
+      <h4 className='subheading1' className='pick-destination'>
         <span>01</span> pick your destination
       </h4>
-      <article className='img-container'>
-        <img className='destination-img' src={img} alt='destination' />
-      </article>
-      <article className='destination-info-container'>
-        <div className='btns-container'>
-          {buttons.map((b, index) => {
-            return (
-              <button className='destination-btn' key={index}>
-                <p className='subheading2'>{b}</p>
-              </button>
-            );
-          })}
-        </div>
-        <h1>{name}</h1>
-        <p>{description}</p>
-        <div className='destination-line'></div>
-        <div className='distance-time-container'>
-          <div className='distance-container'>
-            <p className='subheading2'>avg. distance</p>
-            <p className='subheading1'>{distance}</p>
+      <div className='destination-page-inner-container'>
+        <article className='img-container'>
+          <img className='destination-img' src={img} alt='destination' />
+        </article>
+        <article className='destination-info-container'>
+          <div className='btns-container'>
+            {buttons.map((b, index) => {
+              return (
+                <button
+                  className={
+                    b === name
+                      ? 'destination-btn active-destination-btn'
+                      : 'destination-btn'
+                  }
+                  key={index}
+                  onClick={() => setActiveDestination(destinations[index])}
+                >
+                  <h5 className='destination-btns-text'>{b}</h5>
+                </button>
+              );
+            })}
           </div>
-          <div className='time-container'>
-            <p className='subheading2'>est. travel time</p>
-            <p className='subheading1'>{travel}</p>
+          <h2>{name}</h2>
+          <p className='destination-description'>{description}</p>
+          <div className='destination-line'></div>
+          <div className='distance-time-container'>
+            <div className='distance-container'>
+              <p className='subheading2 distance-container-text1'>
+                avg. distance
+              </p>
+              <p className='subheading1 distance-container-text2'>{distance}</p>
+            </div>
+            <div className='time-container'>
+              <p className='subheading2 distance-container-text1'>
+                est. travel time
+              </p>
+              <p className='subheading1 distance-container-text2'>{travel}</p>
+            </div>
           </div>
-        </div>
-      </article>
+        </article>
+      </div>
     </section>
   );
 }
